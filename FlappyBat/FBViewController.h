6ff5535aea37b273916081939e0d1ceefe2c0b21
Jake Heiser <jake@quizlet.com>
2014-02-13 11:16:33 -0600

    Fixed header file

--- FlappyBat/FBViewController.h
+++ FlappyBat/FBViewController.h
@@ -11,12 +11,10 @@
 @interface FBViewController : UIViewController
 
 @property (strong, nonatomic) UIView *bat;
-@property (strong, nonatomic) UILabel *startLabel;
 @property (strong, nonatomic) UILabel *counterLabel;
+@property (strong, nonatomic) NSMutableArray *blocks;
 
 @property (assign, nonatomic) CGFloat batVelocity;
-
-@property (strong, nonatomic) NSMutableArray *blocks;
 
 @property (strong, nonatomic) CADisplayLink *displayLink;
 @property (strong, nonatomic) NSTimer *blockTimer;
